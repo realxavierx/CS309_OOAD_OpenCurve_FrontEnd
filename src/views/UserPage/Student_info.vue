4e2f03c44ba35a414e2399e181d20c8766197959
Young JJ <91972567+Vicavier@users.noreply.github.com>
2023-01-05 21:45:57 +0800

    front-back-connected

--- src/views/UserPage/Student_info.vue
+++ src/views/UserPage/Student_info.vue
@@ -3,10 +3,7 @@
     <div class="general-info-t">
       <div class="general-info-avatar">
         <!-- <img :src="baseInfo.avatar_url" alt=""> -->
-        <img src="../../img/123.jpg" alt="">
-        <div class="avatar-hover">
-          <img src="../../img/avatar-hover.png" alt="">
-        </div>
+        <img :src="baseInfo.avatar_url" alt="">
       </div>
       <div class="general-info-t-info">
         <div class="name">{{ baseInfo.nick_name }}</div>
@@ -44,13 +41,14 @@
         <el-form ref="form" :model="form" label-width="80px" size="mini">
           <el-form-item label="上传头像">
             <el-upload
-                class="avatar-uploader"
-                action="https://jsonplaceholder.typicode.com/posts/"
-                :show-file-list="false"
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              class="avatar-uploader"
+              :action="uploadActionUrl"
+              accept="image/png, image/jpg, image/gif"
+              :on-success="handleAvatarSuccess"
+              :limit="1">
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+              <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
             </el-upload>
           </el-form-item>
           <el-form-item label="用户昵称">
@@ -101,7 +99,7 @@
             <el-input type="textarea" v-model="form.desc" placeholder="主人很懒，还没有简介噢~"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">提交修改</el-button>
+            <el-button type="primary" @click="getInfo();">提交修改</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -117,6 +115,8 @@ export default {
   data() {
     return {
       options: regionData,
+      imageUrl: '',
+      uploadActionUrl:'http://localhost:8081/cloud_storage/file/uploading',
       form: {
         name: '教父爷爷',
         birthday: '2001-09-24',
@@ -128,51 +128,69 @@ export default {
         desc: ''
       },
       baseInfo: {
-        nick_name: '教父爷爷',
-        email: sessionStorage.getItem(''),
+        nick_name: '未登录',
+        email: localStorage.getItem('USER_ID'),
         avatar_url: '',
-        money: 114514,
-        birthday: '2001-09-24',
-        collage: '树仁书院',
-        major: '计算机科学与工程',
-        gender: '男',
-        region: '广东省-深圳市-南山区',
-        address: '广东省-深圳市-南山区-桃园街道1088号-南方科技大学',
-        type: '学生'
+        money: 0,
+        birthday: '未填写',
+        collage: '未填写',
+        major: '未填写',
+        gender: '未填写',
+        region: '未填写',
+        address: '未填写',
+        type: localStorage.getItem('type')
       }
     }
   },
   methods: {
-    initial() {
+    getInfo() {
       this.axios({
-        method: 'POST',
-        url: 'http://10.24.233.83:8080/education/register/getVertiCode',
-        data: {
-          key: sessionStorage.getItem('SESSION_KEY')
-        },
-        transformRequest: [function (data) {
-          let str = '';
-          for (let key in data) {
-            str += encodeURIComponent(key) + '=' + encodeURIComponent(data[key]) + '&';
-          }
-          return str;
-        }]
-      }).then(res => {
-        if (res.status === 200) {
-          if (res.nick_name === '###') {
-            this.baseInfo.nick_name = res.email.substr(0, 8)
-          }
-        }
+        method: 'GET',
+        url: 'http://localhost:8080/education/Info/getInfo/?id=' + this.baseInfo.email,
+      }).then(response => {
+        console.log(response.data.data.Info)
+        this.baseInfo.nick_name = response.data.data.Info.name
+        this.baseInfo.avatar_url = response.data.data.Info.picture_url
+        this.baseInfo.birthday = response.data.data.Info.birthday
+        this.baseInfo.collage = response.data.data.Info.collage
+        this.baseInfo.major = response.data.data.Info.department
+        this.baseInfo.gender = response.data.data.Info.gender
+        this.baseInfo.region = response.data.data.Info.region
+        this.baseInfo.address = response.data.data.Info.address
       })
+      
+      // var t = localStorage.getItem('type')
+      // if (t == 'Student') {
+      //   this.type = "学生"
+      // } else if (t == 'Teacher') {
+      //   this.type = "教师"
+      // } else {
+      //   this.type = "管理员"
+      // }
+      
     },
     toVIP() {
       this.$router.push({
         path: '/userpage/student/vip',
       })
-    }
+    },
+    handleAvatarSuccess(resp) {
+      console.log(resp)
+      this.imageUrl = resp.data.url
+    },
+    // beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+    //   if (rawFile.type !== 'image/jpeg') {
+    //     ElMessage.error('Avatar picture must be JPG format!')
+    //     return false
+    //   } else if (rawFile.size / 1024 / 1024 > 2) {
+    //     ElMessage.error('Avatar picture size can not exceed 2MB!')
+    //     return false
+    //   }
+    //   return true
+    // }
   },
   mounted() {
-
+    this.getInfo()
   }
 }
 </script>
@@ -294,30 +312,31 @@ export default {
   margin: 0 auto;
 }
 
+
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+
 .avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
+  border: 1px dashed var(--el-border-color);
   border-radius: 6px;
   cursor: pointer;
   position: relative;
   overflow: hidden;
+  transition: var(--el-transition-duration-fast);
 }
 
 .avatar-uploader .el-upload:hover {
-  border-color: #409EFF;
+  border-color: var(--el-color-primary);
 }
 
-.avatar-uploader-icon {
+.el-icon.avatar-uploader-icon {
   font-size: 28px;
   color: #8c939d;
   width: 178px;
   height: 178px;
-  line-height: 178px;
   text-align: center;
-}
-
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
 }
 </style>
